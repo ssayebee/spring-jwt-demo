952ca382417711eecbc292d69b60ca7a5a15b841
ssayebee <ssayebee@gmail.com>
2020-10-11 01:49:39 +0900

    Test(H2), Main(mysql) 개발환경 분리

--- src/test/java/com/sangyeop/demojwt/DemojwtApplicationTests.java
+++ src/test/java/com/sangyeop/demojwt/DemojwtApplicationTests.java
@@ -2,12 +2,14 @@ package com.sangyeop.demojwt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@SpringBootTest
 class DemojwtApplicationTests {
 
     @Test
     void contextLoads() {
-        DemojwtApplication.main(new String[]{});
     }
 
 }
