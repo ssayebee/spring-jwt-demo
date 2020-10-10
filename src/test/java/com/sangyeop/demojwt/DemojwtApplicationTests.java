90f722a5c8f7ef623909a9e5c271de49bb459a9a
ssayebee <ssayebee@gmail.com>
2020-10-10 19:30:28 +0900

    Initial commit

---
+++ src/test/java/com/sangyeop/demojwt/DemojwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangyeop.demojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class DemojwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+        DemojwtApplication.main(new String[]{});
+    }
+
+}
